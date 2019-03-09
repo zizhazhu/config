6811ef75060ef433e5c042b62eb3de5dd4e450ba
zizhazhu <zizhazhu@gmail.com>
2019-03-09 13:02:57 +0800

    add tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 if [ -f "$HOME/.bashrc" ]; then
+    echo "" >> $HOME/.bashrc
+    echo "# myself shell config" >> $HOME/.bashrc
     echo "source $(pwd)/shell/init.sh" >> $HOME/.bashrc
 fi
 
 if [ -f "$HOME/.vimrc" ]; then
+    echo "" >> $HOME/.vimrc
     echo "source $(pwd)/vim/vimrc" >> $HOME/.vimrc
+fi
+
+if [ -f "$HOME/.tmux.conf" ]; then
+    echo "" >> $HOME/.tmux.conf
+    echo "mytmux=$(pwd)" >> $HOME/.tmux.conf
+    echo "source $mytmux/.tmux.conf" >> $HOME/.tmux.conf
 fi
