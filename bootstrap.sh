17c8a324564fcf8057559d43a8720c16cf3389a4
zizhazhu <zizhazhu@gmail.com>
2020-03-11 12:42:27 +0800

    fix format; add plug.vim install

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,17 +8,17 @@ fillConfig() {
     echo $1
 
     if [ -f $1 ]; then
-      grep "$begin" $1 &> /dev/null
-      if [ $? != 0 ]; then
-          echo "" >> $1
-          echo "$begin" >> $1
-          echo "$2" >> $1
-          echo "$end" >> $1
-      else
-          echo "$1 already exists"
-      fi
+        grep "$begin" $1 &> /dev/null
+        if [ $? != 0 ]; then
+            echo "" >> $1
+            echo "$begin" >> $1
+            echo "$2" >> $1
+            echo "$end" >> $1
+        else
+            echo "$1 already exists"
+        fi
     else
-      echo "$1 not exist"
+        echo "$1 not exist"
     fi
 }
 
@@ -33,3 +33,14 @@ fillConfig "$HOME/.zshrc" "source $dirnow/shell/init.sh" "# myself config"
 fillConfig $vimrc "source $dirnow/vim/vimrc" "\" myself config"
 fillConfig $tmuxconf "source $dirnow/tmux/tmux.conf" "# myself config"
 
+if [ -x "$(command -v curl)" ]; then
+    if [ -f "$HOME/.vim/autoload/plug.vim" ]; then
+        echo "plug.vim already exists"
+    else
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        echo "plug.vim installed."
+    fi
+else
+    echo "Please install curl."
+fi
