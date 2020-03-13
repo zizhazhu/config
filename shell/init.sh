4b4b38cb0e2e6fb90190aa2de8818e90e9d24696
zizhazhu <zizhazhu@gmail.com>
2020-03-13 18:48:44 +0800

    add env CONFIG_PATH

--- shell/init.sh
+++ shell/init.sh
@@ -1,9 +1,26 @@
+# clear up repeated path in PATH
+if [ -n "$PATH" ]; then
+	old_PATH=$PATH:; PATH=
+	while [ -n "$old_PATH" ]; do
+		x=${old_PATH%%:*}      
+		case $PATH: in
+			*:"$x":*) ;;         
+			*) PATH=$PATH:$x;;  
+		esac
+		old_PATH=${old_PATH#*:}
+	done
+	PATH=${PATH#:}
+	unset old_PATH x
+fi
+
 # prevent from loading this script twice
 if [ -z "$_INIT_SH_LOADED" ]; then
 	_INIT_SH_LOADED=1
 else
 	return
 fi
+
+export CONFIG_PATH=$(dirname $0)/../
 
 # $- shows the current set of options in your current shell
 # H - histexpand
@@ -21,17 +38,3 @@ if [ -d "$HOME/.local/bin" ]; then
 	export PATH="$HOME/.local/bin:$PATH"
 fi
 
-# clear up repeated path in PATH
-if [ -n "$PATH" ]; then
-	old_PATH=$PATH:; PATH=
-	while [ -n "$old_PATH" ]; do
-		x=${old_PATH%%:*}      
-		case $PATH: in
-			*:"$x":*) ;;         
-			*) PATH=$PATH:$x;;  
-		esac
-		old_PATH=${old_PATH#*:}
-	done
-	PATH=${PATH#:}
-	unset old_PATH x
-fi
